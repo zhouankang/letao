8cc82fb9bdb8719f68688203136afbb0c88b777f
zhouankang <1655802143@qq.com>
2018-03-03 14:17:52 +0800

    进度条完成

---
+++ public/pc/js/common.js
@@ -0,0 +1,15 @@
+$(function(){
+    //禁用进度环；
+    // NProgress.configure({ showSpinner: false });
+
+
+  //NProgress是一个全局事件；给document 注册；
+  $(document).ajaxStart(function(){
+      NProgress.start();
+  })
+
+  $(document).ajaxStop(function(){
+     NProgress.done();
+  })
+      
+})
