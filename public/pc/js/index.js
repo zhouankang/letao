1bf0b32ac2e862bd5e445c5317994a4b42ca205f
zhouankang <1655802143@qq.com>
2018-03-03 18:40:16 +0800

    侧边栏显示与隐藏完成

--- public/pc/js/index.js
+++ public/pc/js/index.js
@@ -1,5 +1,21 @@
-// $(function(){
-//     //找到所有的li；
-//     var lis=document.querySelectorAll(".nav>ul>li");
- 
-// })
+$(function(){
+
+    // 点击分类管理展开或隐藏
+  $(".nav_two").prev().on("click",function(){
+     $(this).next().slideToggle();
+  });
+
+
+  //点击meun按钮；侧边栏展开或隐藏；
+  $(".muen").on("click",function(){
+    //   console.log("222")
+      $(".lt_aside").toggleClass("now");
+     $(".lt_main").toggleClass("now");
+     $(".header").toggleClass("now");
+  })
+
+  // 退出功能
+   $(".logout").on("click",function(){
+      $("#modalshow").modal("show");
+   })
+})
