00cbe55a5371bb97893be9c43982ec3cb0831b3c
zhouankang <1655802143@qq.com>
2018-03-06 23:12:25 +0800

    首页完成

---
+++ public/back/js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 陈祖武 on 2018/3/6.
+ */
+$(function(){
+    //初始化滑动插件
+    mui('.mui-scroll-wrapper').scroll({
+
+    });
+//获得slider插件对象
+    //获得slider插件对象
+    var gallery = mui('.mui-slider');
+    gallery.slider({
+        interval:2000//自动轮播周期，若为0则不自动播放，默认为0；
+    });
+});
