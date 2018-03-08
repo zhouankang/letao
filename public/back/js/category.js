5f7f767db8d64ac40f8dd250fb4d3de83e004404
zhouankang <1655802143@qq.com>
2018-03-08 18:41:57 +0800

    分类页回调渲染

--- public/back/js/category.js
+++ public/back/js/category.js
@@ -7,14 +7,12 @@ $(function(){
             type:"GET",
             url:"/category/queryTopCategory",
             success:function(info){
+                console.log(info);
                 $(".nav").html(template("tmp_first",info));
+                render(info.rows[0].id);
             }
         });
-
-    //根据i渲染二级
-    $(".nav").on("click","li",function(){
-        $(this).addClass("now").siblings().removeClass("now");
-        var id=$(this).data("id");
+    var render=function(id){
         $.ajax({
             type:"get",
             url:"/category/querySecondCategory",
@@ -22,11 +20,17 @@ $(function(){
                 id:id
             },
             success:function(info){
-                console.log(info)
-                $(".product").html(template("tmp_second",info))
+                console.log(info);
+                $(".product").html(template("tmp_second",info));
             }
         })
+    };
 
+    //根据i渲染二级
+   $(".nav").on("click","li",function(id){
+        $(this).addClass("now").siblings().removeClass("now");
+        var id=$(this).data("id");
+       render(id)
     })
 
 });
